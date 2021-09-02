55ff9373f3f3eaecba12d19d918cd33ff12f2f33
Princess <princesstheaenriquez@gmail.com>
2021-09-01 22:59:28 -0500

    Added multiple rounds.

--- assets/game.js
+++ assets/game.js
@@ -12,6 +12,7 @@ var fight = function(enemyNames) {
     // repeat and execute as long as the enemy-robot is still alive
     while(playerHealth > 0 && enemyHealth > 0) {
     var promptFight = window.prompt("Would you like to FIGHT or SKIP this battle? Enter FIGHT or SKIP to choose.");
+
     
     // if player picks "skip" confirm and thens stop the loop
     if (promptFight === "skip" || promptFight === "SKIP") {
@@ -65,10 +66,23 @@ var fight = function(enemyNames) {
 } 
 };
 
-
+// fight each enemy-robot by looping over them and fighting them one at a time
 for (var i = 0; i < enemyNames.length; i++) {
-    var pickedEnemyName = enemyNames[i];
-    enemyHealth = 50;
+    //if player is still alive, keep fighting
+    if (playerHealth>0) {
+        //let player know what round they are in, remember that arrays starts at 0 so it needs to have 1 added to it
+        window.alert("Welcome to Robot Gladiators! Round " + (i + 1));
+        // pick new enemy to fight based on the index of the enemyNames array
+        var pickedEnemyName = enemyNames[i];
+        // reset enemyHealth before starting a new fight
+        enemyHealth = 50;
+        // use debugger to pause script from running and checking what is going on at that moment in the code
+    }
+    else {
+        window.alert("You have lost your robot in battle! Game over!");
+        break;
+    }
+
     // call fight function with enemy-robot
     fight(pickedEnemyName);
 }
